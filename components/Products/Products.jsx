eb6be36d3230064aeda8c957ad63b5a7852985cf
ulrichschulzendorf <ulrich.s.schulzendorf@gmail.com>
2021-06-01 15:37:04 +0200

    products added

---
+++ components/Products/Products.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Grid from "@material-ui/core";
+
+const products = [
+  { id: 1, name: "Schuhe", description: "sehr schöne Schuhe" },
+  { id: 2, name: "Mütze", description: "sehr schöne Mütze" },
+  { id: 3, name: "Dachpappe", description: "sehr schöne schwarze Dachpappe" },
+];
+
+const Products = () => {
+  <main>
+    <Grid container justify="center" spacing={4}></Grid>
+  </main>;
+};
+
+export default Products;
