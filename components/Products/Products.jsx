cc277da98c6a0a647dacf13dc2a136046c746951
ulrichschulzendorf <ulrich.s.schulzendorf@gmail.com>
2021-06-09 14:42:19 +0200

    added first styles

--- components/Products/Products.jsx
+++ components/Products/Products.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import Grid from "@material-ui/core";
 
 const products = [
-  { id: 1, name: "Schuhe", description: "sehr schöne Schuhe" },
-  { id: 2, name: "Mütze", description: "sehr schöne Mütze" },
-  { id: 3, name: "Dachpappe", description: "sehr schöne schwarze Dachpappe" },
+  { id: 1, name: "Schuhe", description: "sehr schöne Schuhe", price: "€7" },
+  { id: 2, name: "Mütze", description: "sehr schöne Mütze", price: "€5" },
+  {
+    id: 3,
+    name: "Dachpappe",
+    description: "sehr schöne schwarze Dachpappe",
+    price: "€4",
+  },
 ];
 
 const Products = () => {
   <main>
-    <Grid container justify="center" spacing={4}></Grid>
+    <Grid container justify="center" spacing={4}>
+      {products.map((product) => (
+        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+          <Product product={product} />
+        </Grid>
+      ))}
+    </Grid>
   </main>;
 };
 
